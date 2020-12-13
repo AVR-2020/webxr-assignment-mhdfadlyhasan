81184ffe0d264f976b10e83b2c77f83b01f839b6
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-12-13 16:49:00 +0700

    Add following feature

--- back-end-typevritter/database/dbfollow.js
+++ back-end-typevritter/database/dbfollow.js
@@ -1,1 +1,20 @@
-const {sequelize,DataTypes} = require('./connect')
+const { sequelize, DataTypes } = require('./connect')
+const Follow = sequelize.define('follow', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  following_user: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  to_follow_user: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {})
+
+// `sequelize.define` also returns the model
+console.log(Follow === sequelize.models.Follow ? 'created ' : 'za heck') // true
+module.exports = Follow
--- back-end-typevritter/routes/follow.js
+++ back-end-typevritter/routes/follow.js
@@ -1,5 +1,32 @@
 const { router } = require('./index')
-router.get('/follow', function (req, res) {
-  res.send('follow')
+const Follow = require('../database/dbfollow')
+router.post('/follow', function (req, res) {
+  Follow.findOrCreate({
+    where: {
+      following_user: req.body.following_user,
+      to_follow_user: req.body.to_follow_user
+    },
+    defaults: {
+      following_user: req.body.following_user,
+      to_follow_user: req.body.to_follow_user
+    }
+  }).then(result => {
+    const created = result[1] // result[1] is false if entry is exist
+    if (created) res.send('succesfully following')
+    else res.send('Already following')
+  })
+})
+router.post('/unfollow', function (req, res) {
+  Follow.destroy({
+    where: {
+      following_user: req.body.following_user,
+      to_follow_user: req.body.to_follow_user
+    }
+  }).then(result => {
+    if (!result) res.send('Failed to Unfollow!')
+    else res.send('Success')
+  }).catch(error => {
+    res.send(error)
+  })
 })
 module.exports = router
