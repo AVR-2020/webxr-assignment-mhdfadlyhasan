a9fb32102d50270fca17472d751e9266e42b90bc
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-12-13 09:52:20 +0700

    Install necessary package

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const app = express()
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
