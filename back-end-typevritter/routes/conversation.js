6abcdd5d0592c8489275fc7a1aa9a10ee74941c3
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-01 01:10:03 +0700

    Create conversation, almost finish chat

--- back-end-typevritter/routes/conversation.js
+++ back-end-typevritter/routes/conversation.js
@@ -1,44 +1,57 @@
 const { router } = require('./index')
 const { Op } = require('sequelize')
 const Conversation = require('../database/dbconversation')
-router.get('/conversation', function (req, res) {
-  console.log(req.session.user.id)
-  Conversation.findAll({
-    where: {
-      [Op.or]: [
-        { user_1: req.session.user.id },
-        { user_2: req.session.user.id }
-      ]
-    }
-  }).then(function (object) {
-    if (object.length > 0) res.send(object)
-    else res.send('Conversation is empty!')
-  }).catch(function (error) {
-    res.send(error)
-  })
-})
 
-// router.post('/conversation', function (req, res) {
-//   Conversation.findOrCreate({
+router.get('/conversation', function (req, res) {
+  res.render('pages/conversation', { user_id: req.session.user.id })
+})
+// router.get('/all_conversation', function (req, res) {
+//   console.log(req.session.user.id)
+//   Conversation.findAll({
 //     where: {
 //       [Op.or]: [
-//         { user_1: req.body.user_1 },
-//         { user_2: req.body.user_2 }
+//         { user_1: req.session.user.id },
+//         { user_2: req.session.user.id }
 //       ]
-//     },
-//     defaults: {
-//       user_1: req.body.user_1,
-//       user_2: req.body.user_2
 //     }
-//   }).then(result => {
-//     // result[1] is false if entry is exist
-//     if (result[1]) res.send('succesfully creating')
-//     else res.send('Already have new conversation')
+//   }).then(function (object) {
+//     if (object.length > 0) res.send(object)
+//     else res.send('Conversation is empty!')
 //   }).catch(function (error) {
 //     res.send(error)
 //   })
 // })
 
+router.post('/conversation', function (req, res) {
+  Conversation.findOrCreate({
+    where: {
+      [Op.or]: [
+        {
+          [Op.and]: [
+            { user_1: req.body.user_1 },
+            { user_2: req.body.user_2 }
+          ]
+        },
+        {
+          [Op.and]: [
+            { user_2: req.body.user_1 },
+            { user_1: req.body.user_2 }
+          ]
+        }
+      ]
+    },
+    defaults: {
+      user_1: req.body.user_1,
+      user_2: req.body.user_2
+    }
+  }).then(result => {
+    // result[1] is false if entry is exist
+    if (result[1]) res.send('succesfully creating')
+    else res.send('Already have new conversation')
+  }).catch(function (error) {
+    res.send(error)
+  })
+})
 router.delete('/conversation', function (req, res) {
   Conversation.destroy({
     where: {
