0639aa57da60a1a2d7db2bf0a51d1706a95f0e65
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-02 10:50:35 +0700

    add register and login page

--- routes/login.js
+++ routes/login.js
@@ -8,10 +8,12 @@ router.get('/login', function (req, res) {
 router.post('/login', function (req, res) {
   User.findOne({ where: { name: req.body.name, password: req.body.password } }).then(function (object) {
     if (object) {
+      console.log(object)
       req.session.user = object
-      res.redirect('/home')
-    } else res.send('wrong login creditensials')
+      res.send('success')
+    } else res.send('wrong login!')
   }).catch(function (error) {
+    console.log(error)
     res.send(error)
   })
 })
